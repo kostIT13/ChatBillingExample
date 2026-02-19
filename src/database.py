b20408e6d2d615a89acc0862551e96aa5b2f21c9
Konstantin Balandin <kostabalandin169@gmail.com>
2026-02-19 17:49:28 +0700

    ,

--- src/database.py
+++ src/database.py
@@ -2,15 +2,21 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
+POSTGRES_USER = os.getenv("POSTGRES_USER", "user")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "pass")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")  
+POSTGRES_DB = os.getenv("POSTGRES_DB", "mydb")
 
-DATABASE_URL = "postgresql+asyncpg://user:pass@localhost:5432/mydb"
 
+DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
+
+print(f"Подключение к БД: postgresql+asyncpg://{POSTGRES_USER}:****@{POSTGRES_HOST}:5432/{POSTGRES_DB}")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 AsyncSessionLocal = sessionmaker(
     engine, 
-    class_=AsyncSession, 
+    class_=AsyncSession,
     expire_on_commit=False
 )
 
--- src/models/chat.py
+++ src/models/chat.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from src.database import Base
 
 class Chat(Base):
-
     __tablename__ = "chats"
+    
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
